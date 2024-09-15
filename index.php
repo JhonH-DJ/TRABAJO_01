d3d79ca46d4e53a02c127f18a09f922f5cfe2791
JhonH-DJ <jhonhardy2003@gmail.com>
2024-09-15 00:47:01 -0500

    Mi trabajo casi culminado

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand ms-5" href="#"> 
+        <img src="resources/img/logo.png" alt="" style="width: 35px; margin-left: 20px;"> 
+        TIENDAS Mas
+    </a>
+    <div class="d-flex align-items-center" style="margin-left: auto; margin-right: 70px;">
+        <div class="d-flex align-items-center me-3">
+            <img src="resources/img/wha.png" alt="Icon" class="me-2" style="width: 16px; height: 16px;">
+            <span>Whatsapp</span>
+        </div>
+
+        <div class="d-flex align-items-center">
+            <img src="resources/img/face.png" alt="Icon" class="me-2" style="width: 18px; height: 18px;">
+            <span>Facebook</span>
+        </div>
+    </div>
+</nav>
+
+    <div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-10 col-lg-11">
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="resources/img/slide02.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block text-center">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="resources/img/slide03.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block text-center">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="resources/img/slide01.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block text-center">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    <div class="h4 text-center mt-4">Nuestros Productos</div>
+
+    <div class="container">
+  <div class="row">
+    <div class="col-md-6 mb-3 mt-4">
+      <div class="card">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="resources/img/card01.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 mb-3 mt-4">
+      <div class="card">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="resources/img/card02.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 mb-3">
+      <div class="card">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="resources/img/card03.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 mb-3">
+      <div class="card">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="resources/img/card04.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 mb-3">
+      <div class="card">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="resources/img/card05.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-6 mb-3">
+      <div class="card">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="resources/img/card06.jpg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-8 col-md-6 col-lg-12"> 
+            <div class="card">
+                <div class="card-body">
+                <div class="d-flex align-items-center">
+                <img src="resources/img/add.png" alt="Icon" class="me-2" style="width: 16px; height: 16px;">
+                <span>Jr.Amazonas 120</span>
+            </div>
+                <div class="d-flex align-items-center">
+                <img src="resources/img/cal.png" alt="Icon" class="me-2" style="width: 16px; height: 16px;">
+                <span>123456789</span>
+            </div>
+                </div>
+        <div class="container text-center">
+            <p>Todos los derechos reservados 2014</p>
+        </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
